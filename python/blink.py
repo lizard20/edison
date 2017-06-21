6eae583613e8f1548003576cd64d3f0611063c01
lizard20 <anunez20@gmail.com>
2017-06-21 11:39:22 -0500

    update

--- python/blink.py
+++ python/blink.py
@@ -5,6 +5,8 @@
 '''
 import mraa as m
 import time
+import sys
+
 
 # STATES
 OFF = 0 
@@ -13,14 +15,23 @@ ON = 1
 # 13 ~ led DS2
 LED = 13    
 
-# time sleep
+# time
 T =  1.0    # 1.0 ~ 1 sec 
 
-led = m.Gpio ( LED )
-led.dir ( m.DIR_OUT )
+def main():
+    led = m.Gpio ( LED )
+    led.dir ( m.DIR_OUT )
+    print ( "To finish press: Ctrl + c" )
+    try:
+        while True:
+            led.write ( ON )
+            time.sleep ( T )
+            led.write ( OFF )
+            time.sleep ( T )
+    except KeyboardInterrupt:
+        led.write ( OFF )
+        print ( "blink is closing..." )
+        sys.exit ( 0 )
 
-while True:
-    led.write ( ON )
-    time.sleep ( T )
-    led.write ( OFF )
-    time.sleep ( T )
+if __name__ == "__main__":
+    main ()
